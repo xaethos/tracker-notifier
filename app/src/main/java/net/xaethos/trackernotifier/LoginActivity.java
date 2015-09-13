0ebad06fc2e1cf10a0c7e54a157dae3c172071b6
Diego García <xaethos@gmail.com>
2015-09-14 03:54:30 +0800

    Working login call! Wow!

--- app/src/main/java/net/xaethos/trackernotifier/LoginActivity.java
+++ app/src/main/java/net/xaethos/trackernotifier/LoginActivity.java
@@ -3,9 +3,9 @@ package net.xaethos.trackernotifier;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +13,18 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit.MoshiConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * A login screen that offers login via email/password.
@@ -20,16 +32,9 @@ import android.widget.TextView;
 public class LoginActivity extends Activity {
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS =
-            new String[]{"foo@example.com:hello", "bar@example.com:world"
-            };
-    /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private UserLoginTask mAuthTask = null;
+    private Subscription mLoginSubscription;
 
     // UI references.
     private EditText mEmailView;
@@ -74,7 +79,7 @@ public class LoginActivity extends Activity {
      * errors are presented and no actual login attempt is made.
      */
     public void attemptLogin() {
-        if (mAuthTask != null) {
+        if (mLoginSubscription != null && !mLoginSubscription.isUnsubscribed()) {
             return;
         }
 
@@ -89,7 +94,7 @@ public class LoginActivity extends Activity {
         View errorField = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password)) {
+        if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_field_required));
             errorField = mPasswordView;
         }
@@ -107,13 +112,42 @@ public class LoginActivity extends Activity {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             errorField.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            return;
         }
+
+        // Show a progress spinner, and kick off a background task to
+        // perform the user login attempt.
+        showProgress(true);
+
+        Retrofit retrofit =
+                new Retrofit.Builder().baseUrl("https://www.pivotaltracker.com/services/v5/")
+                        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                        .addConverterFactory(MoshiConverterFactory.create())
+                        .build();
+        TrackerService service = retrofit.create(TrackerService.class);
+
+        String credentials = email + ":" + password;
+        String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+
+        mLoginSubscription = service.me(auth)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<User>() {
+                    @Override
+                    public void call(User user) {
+                        Toast.makeText(LoginActivity.this,
+                                "Welcome back " + user.name,
+                                Toast.LENGTH_LONG).show();
+                        showProgress(false);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable error) {
+                        Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_LONG)
+                                .show();
+                        showProgress(false);
+                    }
+                });
     }
 
     private boolean isEmailValid(String email) {
@@ -150,61 +184,18 @@ public class LoginActivity extends Activity {
                 });
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
+    public static class User {
+        long id;
+        String api_token;
+        String email;
+        String name;
+        String initials;
     }
+
+    public interface TrackerService {
+        @GET("me")
+        Observable<User> me(@Header("Authorization") String authorization);
+    }
+
 }
 
